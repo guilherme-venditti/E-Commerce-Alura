e2052237a5068ab185bb518e9d3a7ffccc87759e
Guilherme Venditti <112132085+guilherme-venditti@users.noreply.github.com>
2022-10-31 13:08:33 -0300

    Estrutura básica das páginas criada
    Inserção do header e footer

---
+++ assets/js/Produto.js
@@ -0,0 +1,9 @@
+class Produto {
+    constructor(imagem, categoria, nome, preco, descricao) {
+        this.imagem = imagem;
+        this.categoria = categoria;
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+}
