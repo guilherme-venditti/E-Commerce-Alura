ed1efa7e49f02bc3e1473c548517791e24187ab2
Guilherme Venditti <112132085+guilherme-venditti@users.noreply.github.com>
2022-12-12 21:18:29 -0300

    Live 2 parte 1

---
+++ assets/js/bases.js
@@ -0,0 +1,19 @@
+/*  
+ <div class="produtos__secao">
+    <div class="produto__secao-titulo">
+        <p class="secao__nome"></p>
+        <a href="#" class="secao__verTudo">Ver Tudo</a>
+    </div>
+    <div class="secao__items">
+
+
+        <div class="produto">
+            <img src="" class="produto__img">
+            <p class="produto__nome"></p>
+            <p class="produto__preco"></p>
+            <a href="#" class="produto__verProduto"></a>
+        </div> 
+
+    
+    </div> 
+</div> */ 
